ffe9a7e156f0926f099d29b75b49d00dd02e42ee
gordonmotsinger <gordon.motsinger@gmail.com>
2015-03-19 17:51:23 -0400

    added form for adding employee email

--- app/controllers/employee_email_managers_controller.rb
+++ app/controllers/employee_email_managers_controller.rb
@@ -2,14 +2,26 @@ class EmployeeEmailManagersController < ApplicationController
 
 
   def index
-    @quotes = Quote.all
-    @quote = Quote.new
+    @emails = EmployeeEmail.all
+    @email = EmployeeEmail.new
   end
 
   def create
+    @email = EmployeeEmail.new(email_params)
+
+    if @email.save
+      flash[:notice] = 'Your email information has been added'
+      redirect_to employee_email_manager_path
+    else
+      render action: 'new'
+     end
   end
 
   def delete
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to employee_email_manager_path, notice: 'Line-item was successfully removed.' }
+    end
   end
 
 end
