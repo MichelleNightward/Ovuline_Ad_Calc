062d49a42a7cf7772eabde3e800ade9f5802a2d5
gordonmotsinger <gordon.motsinger@gmail.com>
2015-03-19 17:03:16 -0400

    edited routes to include path to index for the employee_email_manager

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :companies
 
   get "/employees/", to: 'employees#index', as: 'employee'
+  get "/employee_email_managers/", to: 'employee_email_managers#index', as 'employee_email_manager'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
